f966ae16ac35eca3793a8c3eefb84709808e25e0
JessicaMP <jessica95.mp@gmail.com>
2017-11-19 19:17:53 -0500

    Versión 0.0.2

--- js/app.js
+++ js/app.js
@@ -13,4 +13,23 @@ close.addEventListener('click', function(event) {
   containerList.classList.remove('hide');
   display.classList.add('hide');
   display.classList.remove('show');
+  containerList.style.transform = 'translateY(-80%)';
+});
+
+var text = document.getElementById('text');
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(event) {
+  var displayTwo = document.getElementsByClassName('display-two')[0];
+  containerList.classList.add('hide');
+  display.classList.add('hide');
+  displayTwo.classList.add('show');
+  displayTwo.classList.remove('hide');
+
+  var brother = document.getElementsByClassName('display-two')[0].children[0];
+  var content = document.createElement('p');
+  content.textContent = text.value;
+  content.classList.add('sister');
+  displayTwo.insertBefore(content, brother);
+
+  text.value = '';
 });
